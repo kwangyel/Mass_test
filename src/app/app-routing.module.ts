10c09bdfa4fb217fc21d93a44ce60d7f9fe2d98e
kwangyel <kwangyel@gmail.com>
2021-01-22 00:34:29 +0600

    theme change. Select zone feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
 import { SelectZoneComponent } from './select-zone/select-zone.component';
 
 
 const routes: Routes = [
-  {path: '',component: SelectZoneComponent}
+  {path: '',component: SelectZoneComponent},
+  {path: 'map',component: MapComponent},
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: "https://zhichar.ddnsfree.com/cpi"
+  API_URL: "https://zhichar.ddnsfree.com/api"
 };
 
 /*
