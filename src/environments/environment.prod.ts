5a42e0c3d84c9b890c97ccfa09aa97dcb97f9fbe
PPSCoders <kunzwangdi@gmail.com>
2021-06-07 16:59:48 +0600

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: "https://zhichar.ddnsfree.com/mass"
+  API_URL: "https://zhichar.ddnsfree.com/mass",
+  
+  HPI_URL: "https://zhichar.ddnsfree.com/hpi"
 };
